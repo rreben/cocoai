546fc6b7009e013ea5afc199aa1d0060a40dcf30
Rupert Rebentisch <Rupert.rebentisch@gmail.com>
2023-09-21 21:43:49 +0200

    Added cocoai.py

---
+++ cocoai.py
@@ -0,0 +1,32 @@
+import ast
+import os
+
+
+class FunctionCounter(ast.NodeVisitor):
+    def __init__(self):
+        self.function_count = 0
+
+    def visit_FunctionDef(self, node):
+        self.function_count += 1
+        self.generic_visit(node)
+
+
+# Pfad zum geklonten Repository
+repo_path = "<path-to-repo>"
+
+function_count = 0
+
+for foldername, subfolders, filenames in os.walk(repo_path):
+    for filename in filenames:
+        if filename.endswith(".py"):
+            file_path = os.path.join(foldername, filename)
+
+            with open(
+                    file_path, "r", encoding="utf-8", errors="ignore") as file:
+                tree = ast.parse(file.read(), filename=file_path)
+
+                counter = FunctionCounter()
+                counter.visit(tree)
+                function_count += counter.function_count
+
+print(f"Number of functions: {function_count}")
